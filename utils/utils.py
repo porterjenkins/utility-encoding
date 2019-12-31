a723812f76a6c76d804bc20379e9fe41a3c634d9
Porter <porter.jenkins@deliciousai.com>
2019-12-31 15:02:30 -0500

    Add utility functions for reading in Amazon dataset from tar file

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,10 @@
 import numpy as np
+import gzip
+import pandas as pd
+
+true = True
+false = False
+
 
 
 def read_ratings(fname):
@@ -11,3 +17,16 @@ def read_ratings(fname):
             raw_data.append(row)
 
     return np.array(raw_data)
+
+
+def parse(path):
+    g = gzip.open(path, 'rb')
+    for line in g:
+        yield eval(line)
+def pandas_df(path):
+    i = 0
+    df = {}
+    for d in parse(path):
+        df[i] = d
+        i += 1
+    return pd.DataFrame.from_dict(df, orient='index')
