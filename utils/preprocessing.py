7d44dd49951c858bbfd7f889227e2ef467851379
Porter <porterjenkins@gmail.com>
2020-01-01 15:07:15 -0500

    Add function to map user and item ids to ints

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -15,3 +15,24 @@ def split_train_test_user(X, y, test_size=.2):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, stratify=X['user_id'])
 
     return X_train, X_test, y_train, y_test
+
+
+def map_ids_to_idx(df):
+    user_map = {}
+    item_map = {}
+
+    user_cntr = 0
+    item_cntr = 0
+    
+
+
+    for idx, row in df.iterrows():
+
+        if row.user_id not in user_map:
+            user_map[row.user_id] = user_cntr
+            user_cntr += 1
+
+        if row.item_id not in item_map:
+            user_cntr
+
+
