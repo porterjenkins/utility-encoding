6cb4ca82832ec8befa3ddf17b1ea34ed90a794d9
Porter <porterjenkins@gmail.com>
2020-01-03 15:57:32 -0500

    remove of-the-shelf wide and deep module

--- pytorch_widedeep/preprocessing/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._preprocessors import WidePreprocessor
-from ._preprocessors import DeepPreprocessor
-from ._preprocessors import TextPreprocessor
-from ._preprocessors import ImagePreprocessor
--- pytorch_widedeep/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .dense_utils import *
-from .text_utils import *
-from .fastai_transforms import *
-from .image_utils import *
