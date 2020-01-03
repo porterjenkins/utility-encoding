33456e0ecb8e7ec496c372e8d5289320cd3c9aec
Porter <porterjenkins@gmail.com>
2020-01-03 16:45:23 -0500

    Pretrained w&d class

--- model/wide_and_deep.py
+++ model/wide_and_deep.py
@@ -14,6 +14,68 @@ from preprocessing.utils import split_train_test_user, load_dict_output
 import pandas as pd
 from model._loss import loss_mse
 
+class WideAndDeepPretrained(nn.Module):
+
+    def __init__(self, n_items, h_dim_size, wide, wide_dim, fc1=64, fc2=32,):
+        super(WideAndDeepPretrained, self).__init__()
+        self.n_items = n_items
+        self.h_dim_size = h_dim_size
+
+
+        self.embedding = EmbeddingGrad(n_items, h_dim_size)
+        self.fc_1 = nn.Linear(h_dim_size, fc1)
+        self.fc_2 = nn.Linear(fc1, fc2)
+
+        self.wide = EmbeddingGrad(n_items, wide_dim, init_embed=wide)
+
+
+
+        self.output_layer = nn.Linear(wide_dim + fc2, 1)
+
+    def get_input_grad(self, indices):
+        """
+        Get gradients with respect to inputs
+        :param indices: (ndarray) array of item indices
+        :return: (tensor) tensor of gradients with respect to inputs
+        """
+        if indices.ndim == 1:
+            indices = indices.reshape(-1, 1)
+
+
+        dims = [d for d in indices.shape] + [1]
+        idx_tensor = torch.LongTensor(indices).reshape(dims)
+
+        grad = self.embedding.get_grad(indices)
+        grad_at_idx = torch.gather(grad, -1, idx_tensor)
+        return torch.squeeze(grad_at_idx)
+
+
+    def _forward_set(self, x):
+        h = self.embedding(x)
+        h = F.relu(self.fc_1(h))
+        h = F.relu(self.fc_2(h))
+
+        wide = self.wide(x)
+        h = torch.cat([h, wide], dim=-1)
+
+        y_hat = self.output_layer(h)
+        return y_hat
+
+
+    def forward(self, x, x_c=None, x_s=None):
+
+        y_hat = self._forward_set(x)
+
+        if x_c is not None and x_s is not None:
+
+            y_hat_c = self._forward_set(x_c)
+            y_hat_s = self._forward_set(x_s)
+
+            return y_hat, torch.squeeze(y_hat_c), torch.squeeze(y_hat_s)
+
+        else:
+
+            return y_hat
 
 class WideAndDeep(nn.Module):
 
@@ -31,22 +93,50 @@ class WideAndDeep(nn.Module):
 
         self.output_layer = nn.Linear(n_items + fc2, 1)
 
+    def get_input_grad(self, indices):
+        """
+        Get gradients with respect to inputs
+        :param indices: (ndarray) array of item indices
+        :return: (tensor) tensor of gradients with respect to inputs
+        """
+        if indices.ndim == 1:
+            indices = indices.reshape(-1, 1)
+
+
+        dims = [d for d in indices.shape] + [1]
+        idx_tensor = torch.LongTensor(indices).reshape(dims)
+
+        grad = self.embedding.get_grad(indices)
+        grad_at_idx = torch.gather(grad, -1, idx_tensor)
+        return torch.squeeze(grad_at_idx)
+
+
     def _forward_set(self, x):
         h = self.embedding(x)
         h = F.relu(self.fc_1(h))
         h = F.relu(self.fc_2(h))
 
         wide = self.embedding._collect(x)
-        h = torch.cat([h, wide], dim=1)
+        h = torch.cat([h, wide], dim=-1)
 
         y_hat = self.output_layer(h)
         return y_hat
 
 
-    def forward(self, x):
+    def forward(self, x, x_c=None, x_s=None):
 
         y_hat = self._forward_set(x)
-        return y_hat
+
+        if x_c is not None and x_s is not None:
+
+            y_hat_c = self._forward_set(x_c)
+            y_hat_s = self._forward_set(x_s)
+
+            return y_hat, torch.squeeze(y_hat_c), torch.squeeze(y_hat_s)
+
+        else:
+
+            return y_hat
 
 
 
