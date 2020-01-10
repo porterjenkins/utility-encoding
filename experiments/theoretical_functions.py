380485d35075d50a225b7a62dc1b287e82dd2493
Porter <porterjenkins@gmail.com>
2020-01-10 14:51:02 -0500

    theoretical_functions: add command line flag for utility function

--- experiments/theoretical_functions.py
+++ experiments/theoretical_functions.py
@@ -5,15 +5,25 @@ from preprocessing.utils import preprocess_user_item_df
 import numpy as np
 import pandas as pd
 from model.wide_and_deep import WideAndDeep
-from preprocessing.utils import split_train_test_user
+from model.encoder import UtilityEncoder
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import mean_squared_error
+import math
+
+
+UTILITY = sys.argv[1]
+
+print("Running simulation with {} utility".format(UTILITY))
+
 
 RANDOM_SEED = 1990
 N_USERS = 1000
-N = 25
-N_SIM = 5
+N = 64
+N_SIM = 8
+RHO = 2
+
+assert UTILITY in ['cobb-douglas', 'ces']
 
 np.random.seed(RANDOM_SEED)
 
@@ -32,25 +42,32 @@ def mrs_error(M1, M2):
     m1 = M1[idx]
     m2 = M2[idx]
 
-    mse = mean_squared_error(m1, m2)
+    mse = math.sqrt(mean_squared_error(m1, m2))
     return mse
 
-def get_analytical_mrs(w1, w2):
+def get_analytical_cobb_douglas_mrs(w1, w2):
 
     #return w1 + w2
     return -w1/w2
 
-def mrs_mat(x, w):
+def get_analytical_ces_mrs(w1, w2, rho):
+
+    return - (w1 / w2)**(rho-1)
+
+def get_mrs_mat(x, w, mrs_func, rho=None):
     n = x.shape[0]
     mrs_mat = np.zeros((n, n))
 
     for i in range(n):
         for j in range(n):
 
-
-            mrs_mat[i, j] = get_analytical_mrs(w1=w[i], w2=w[j])
+            if rho is None:
+                mrs_mat[i, j] = mrs_func(w1=w[i], w2=w[j])
+            else:
+                mrs_mat[i, j] = mrs_func(w1=w[i], w2=w[j], rho=rho)
 
     return mrs_mat
+
 
 def compute_pariwise_mrs(grad):
 
@@ -59,6 +76,9 @@ def compute_pariwise_mrs(grad):
 
     for i, g_i in enumerate(grad):
         for j, g_j in enumerate(grad):
+
+            if g_j == 0.0:
+                g_j = 1e-3
 
             mrs_mat[i, j] = - (g_i / g_j)
 
@@ -79,6 +99,12 @@ def cobb_douglas(x, w):
 
     return u
 
+def ces(x, w, rho):
+
+    x_power = np.power(x, rho)
+    inner_prod = np.dot(x_power, w)
+    u = np.power(inner_prod, rho)
+    return u
 
 
 def gen_bundle(n, k):
@@ -91,7 +117,7 @@ def gen_bundle(n, k):
     return x, idx
 
 
-def gen_dataset(n_items, n_users, n_reviews):
+def gen_dataset(n_items, n_users, n_reviews, utility_type):
 
     n_samples = n_users * n_reviews
 
@@ -104,7 +130,11 @@ def gen_dataset(n_items, n_users, n_reviews):
         for j in items_reviews_i:
 
             x_vec, x_i = gen_bundle(N, 1)
-            u = cobb_douglas(x_vec, weights)
+
+            if utility_type == 'cobb-douglas':
+                u = cobb_douglas(x_vec, weights)
+            elif utility_type == 'ces':
+                u = ces(x_vec, weights, RHO)
 
             X[cntr, 0] = i
 
@@ -117,18 +147,29 @@ def gen_dataset(n_items, n_users, n_reviews):
     return X, y
 
 
-X, y = gen_dataset(N, N_USERS, N // 2)
+def clip(arr, k):
+
+    n = len(arr)
+    arr.sort()
+    return arr[:n-k]
+
+
+
+X, y = gen_dataset(N, N_USERS, N // 2, utility_type=UTILITY)
 
 print(X.shape)
 
-MRS = mrs_mat(x=np.arange(N), w=weights)
+if UTILITY == 'cobb-douglas':
+    MRS = get_mrs_mat(x=np.arange(N), w=weights, mrs_func=get_analytical_cobb_douglas_mrs)
+else:
+    MRS = get_mrs_mat(x=np.arange(N), w=weights, mrs_func=get_analytical_ces_mrs, rho=RHO)
 
 df = pd.DataFrame(np.concatenate([X, y], axis=1), columns=['user_id', 'item_id', 'rating'])
 df, user_item_rating_map, item_rating_map, user_id_map, id_user_map, item_id_map, id_item_map, stats = preprocess_user_item_df(df)
 
 X = df[['user_id', 'item_id']].astype(np.int64)
 y = df['rating']
-X_train, X_test, y_train, y_test = split_train_test_user(X, y, random_seed=1990)
+#X_train, X_test, y_train, y_test = split_train_test_user(X, y, random_seed=1990)
 
 
 
@@ -137,11 +178,11 @@ X_train, X_test, y_train, y_test = split_train_test_user(X, y, random_seed=1990)
 
 batch_size = 32
 k = 5
-h_dim = 256
-n_epochs = 20
+h_dim = 16
+n_epochs = 10
 lr = 5e-5
-loss_step = 50
-eps = 0.05
+loss_step = 10
+eps = 0.01
 
 output = {"linear": [],
           "vanilla": [],
@@ -151,10 +192,10 @@ for iter in range(N_SIM):
 
     # Train Linear Regression
     enc = OneHotEncoder(sparse=False)
-    X_train_sparse = enc.fit_transform(X=X_train['item_id'].values.reshape(-1,1))
+    X_train_sparse = enc.fit_transform(X=X['item_id'].values.reshape(-1,1))
 
     linear = LinearRegression(fit_intercept=True)
-    linear.fit(X_train_sparse, y_train)
+    linear.fit(X_train_sparse, y)
 
 
 
@@ -165,11 +206,11 @@ for iter in range(N_SIM):
 
 
     # Train Vanilla Wide&Deep
-    wide_deep = WideAndDeep(stats['n_items'],  h_dim_size=246, fc1=64, fc2=32)
+    item_encoder = UtilityEncoder(n_items=stats['n_items'], h_dim_size=h_dim)
 
-    wide_deep.fit(X_train, y_train, batch_size, lr, n_epochs, loss_step, eps)
+    item_encoder.fit(X, y, batch_size, lr, n_epochs, loss_step, eps)
 
-    grad_vanilla = wide_deep.get_input_grad(np.arange(N))
+    grad_vanilla = item_encoder.get_input_grad(np.arange(N))
     mrs_vanilla = compute_pariwise_mrs(grad_vanilla.data.numpy())
     l2_vanilla = mrs_error(MRS, mrs_vanilla)
     output['vanilla'].append(l2_vanilla)
@@ -177,11 +218,11 @@ for iter in range(N_SIM):
 
 
     # Train Neural Utility Function
-    wide_deep_utility = WideAndDeep(stats['n_items'], h_dim_size=246, fc1=64, fc2=32)
-    wide_deep_utility.fit_utility_loss(X_train, y_train, batch_size, lr, n_epochs, loss_step, eps, user_item_rating_map, \
+    item_encoder_utility = UtilityEncoder(n_items=stats['n_items'], h_dim_size=h_dim)
+    item_encoder_utility.fit_utility_loss(X, y, batch_size, lr, n_epochs, loss_step, eps, user_item_rating_map, \
                                 item_rating_map, k, stats['n_items'])
 
-    grad_utility = wide_deep_utility.get_input_grad(np.arange(N))
+    grad_utility = item_encoder_utility.get_input_grad(np.arange(N))
     mrs_utility = compute_pariwise_mrs(grad_utility.data.numpy())
     l2_utility = mrs_error(MRS, mrs_utility)
     output['utility'].append(l2_utility)
@@ -191,6 +232,10 @@ for iter in range(N_SIM):
 for k, v in output.items():
 
     print(k)
-    print(v)
-    print("mean: {:.4f}".format(np.mean(output[k])))
-    print("std: {:.4f}".format(np.std(output[k])))
+
+
+    v_clip = clip(v, k=2)
+
+    print(v_clip)
+    print("mean: {:.4f}".format(np.mean(v_clip)))
+    print("std: {:.4f}".format(np.std(v_clip)))
