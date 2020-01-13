d1bc15bf93622d5dc2544c9bf8061a60369108d1
Porter <porterjenkins@gmail.com>
2020-01-13 14:31:34 -0500

    Add GMF baseline

---
+++ baselines/gmf.py
@@ -0,0 +1,28 @@
+# https://github.com/yihong-chen/neural-collaborative-filtering/blob/master/src/gmf.py
+
+import torch
+
+
+class GMF(torch.nn.Module):
+    def __init__(self, config):
+        super(GMF, self).__init__()
+        self.num_users = config['num_users']
+        self.num_items = config['num_items']
+        self.latent_dim = config['latent_dim']
+
+        self.embedding_user = torch.nn.Embedding(num_embeddings=self.num_users, embedding_dim=self.latent_dim)
+        self.embedding_item = torch.nn.Embedding(num_embeddings=self.num_items, embedding_dim=self.latent_dim)
+
+        self.affine_output = torch.nn.Linear(in_features=self.latent_dim, out_features=1)
+        self.logistic = torch.nn.Sigmoid()
+
+    def forward(self, user_indices, item_indices):
+        user_embedding = self.embedding_user(user_indices)
+        item_embedding = self.embedding_item(item_indices)
+        element_product = torch.mul(user_embedding, item_embedding)
+        logits = self.affine_output(element_product)
+        rating = self.logistic(logits)
+        return rating
+
+    def init_weight(self):
+        pass
