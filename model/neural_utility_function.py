8f64e3e6f28a57604ee28bfa840a19e09ac10982
Porter <porterjenkins@gmail.com>
2020-01-13 11:02:28 -0500

    initial wrapper class for arbitary neural utility func

---
+++ model/neural_utility_function.py
@@ -0,0 +1,61 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import config.config as cfg
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+from model.embedding import EmbeddingGrad
+from preprocessing.utils import split_train_test_user, load_dict_output
+import pandas as pd
+from generator.generator import CoocurrenceGenerator, SimpleBatchGenerator
+from model._loss import loss_mse, utility_loss, mrs_loss
+
+
+
+
+class NeuralUtility(nn.Module):
+
+    def __init__(self, backbone, n_items, h_dim_size):
+        super(NeuralUtility, self).__init__()
+
+        self.embedding = EmbeddingGrad(n_items, h_dim_size)
+        self.backbone = backbone
+
+
+    def get_input_grad(self, indices):
+        """
+        Get gradients with respect to inputs
+        :param indices: (ndarray) array of item indices
+        :return: (tensor) tensor of gradients with respect to inputs
+        """
+        if indices.ndim == 1:
+            indices = indices.reshape(-1, 1)
+
+
+        dims = [d for d in indices.shape] + [1]
+        idx_tensor = torch.LongTensor(indices).reshape(dims)
+
+        grad = self.embedding.get_grad(indices)
+        grad_at_idx = torch.gather(grad, -1, idx_tensor)
+        return torch.squeeze(grad_at_idx)
+
+    def forward(self, x, x_c=None, x_s=None):
+
+        e_i = self.embedding.forward(x)
+        y_hat = self.weights(e_i)
+
+        if x_c is not None and x_s is not None:
+
+            e_c = self.embedding.forward(x_c)
+            e_s = self.embedding.forward(x_s)
+
+            y_hat_c = self.weights(e_c)
+            y_hat_s = self.weights(e_s)
+
+            return y_hat, torch.squeeze(y_hat_c), torch.squeeze(y_hat_s)
+
+        else:
+
+            return y_hat
