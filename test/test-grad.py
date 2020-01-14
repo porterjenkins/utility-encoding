88cf73e4ad9f87e4246d27d4e6d19c96772e20c3
Porter <porterjenkins@gmail.com>
2020-01-14 15:08:16 -0500

    alternative implementation of gradient implementation

--- test/test-grad.py
+++ test/test-grad.py
@@ -9,7 +9,7 @@ print("y", y)
 z = y * y * 3
 print("z", z)
 
-out1 = 10 - z.sum()
+out1 =  z.sum()
 print("out1", out1)
 
 out1.backward(retain_graph=True)
@@ -19,5 +19,10 @@ print("x grad", x.grad)
 out2 = z.mean()
 print("out2", out2)
 
-out2.backward()
+out2.backward(retain_graph=True)
 print("x grad", x.grad)
+
+out3 = z.sum()
+
+dz_dx = torch.autograd.grad(out3, x, create_graph=True)[0]
+print(dz_dx)
