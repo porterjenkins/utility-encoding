4f8931c3f336ae2308f8a66bede9077061a38d43
Porter <porterjenkins@gmail.com>
2020-01-13 22:27:12 -0500

    Implement EmbeddingGrad in GMF

--- baselines/gmf.py
+++ baselines/gmf.py
@@ -1,6 +1,7 @@
 # https://github.com/yihong-chen/neural-collaborative-filtering/blob/master/src/gmf.py
 
 import torch
+from model.embedding import EmbeddingGrad
 
 
 class GMF(torch.nn.Module):
@@ -11,18 +12,19 @@ class GMF(torch.nn.Module):
         self.latent_dim = config['latent_dim']
 
         self.embedding_user = torch.nn.Embedding(num_embeddings=self.num_users, embedding_dim=self.latent_dim)
-        self.embedding_item = torch.nn.Embedding(num_embeddings=self.num_items, embedding_dim=self.latent_dim)
+        #self.embedding_item = torch.nn.Embedding(num_embeddings=self.num_items, embedding_dim=self.latent_dim)
+        self.embedding_item = EmbeddingGrad(num_embedding=self.n_items, embedding_dim=self.latent_dim)
 
         self.affine_output = torch.nn.Linear(in_features=self.latent_dim, out_features=1)
-        self.logistic = torch.nn.Sigmoid()
+        #self.logistic = torch.nn.Sigmoid()
 
     def forward(self, user_indices, item_indices):
         user_embedding = self.embedding_user(user_indices)
         item_embedding = self.embedding_item(item_indices)
         element_product = torch.mul(user_embedding, item_embedding)
         logits = self.affine_output(element_product)
-        rating = self.logistic(logits)
-        return rating
+        #rating = self.logistic(logits)
+        return logits
 
     def init_weight(self):
         pass
