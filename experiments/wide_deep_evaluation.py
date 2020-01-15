f4acd6215c45b52fcf8a0a99c3e4821866e72d28
Porter <porterjenkins@gmail.com>
2020-01-15 12:12:36 -0500

    unify api for evaluations

--- experiments/wide_deep_evaluation.py
+++ experiments/wide_deep_evaluation.py
@@ -10,9 +10,10 @@ from model._loss import loss_mse
 from baselines.gmf import GMF
 from baselines.wide_and_deep import WideAndDeep
 from sklearn.metrics import mean_squared_error
+from experiments.utils import get_eval_metrics
 
 
-train_config = {"loss": 'utility',
+params = {"loss": 'utility',
                 "h_dim_size": 256,
                 "n_epochs": 10,
                 "batch_size": 32,
@@ -38,18 +39,18 @@ stats = load_dict_output(data_dir, "stats.json")
 
 X_train, X_test, y_train, y_test = split_train_test_user(X, y)
 
-wide_deep = WideAndDeep(stats['n_items'], h_dim_size=train_config["h_dim_size"], fc1=64, fc2=32)
+wide_deep = WideAndDeep(stats['n_items'], h_dim_size=params["h_dim_size"], fc1=64, fc2=32)
 
 trainer = NeuralUtilityTrainer(X_train=X_train, y_train=y_train, model=wide_deep, loss=loss_mse, \
-                               n_epochs=train_config['n_epochs'], batch_size=train_config['batch_size'],
-                               lr=train_config["lr"], loss_step_print=train_config["loss_step"],
-                               eps=train_config["eps"], item_rating_map=item_rating_map,
+                               n_epochs=params['n_epochs'], batch_size=params['batch_size'],
+                               lr=params["lr"], loss_step_print=params["loss_step"],
+                               eps=params["eps"], item_rating_map=item_rating_map,
                                user_item_rating_map=user_item_rating_map,
-                               c_size=train_config["c_size"], s_size=train_config["batch_size"],
+                               c_size=params["c_size"], s_size=params["batch_size"],
                                n_items=stats["n_items"])
 
 
-if train_config['loss'] == 'utility':
+if params['loss'] == 'utility':
     trainer.fit_utility_loss()
 else:
     trainer.fit()
@@ -63,15 +64,7 @@ x_test_user = X_test['user_id'].values
 output = pd.DataFrame(np.concatenate([x_test_user.reshape(-1,1), preds.reshape(-1,1), y_test.values.reshape(-1,1)], \
                                     axis=1), columns = ['user_id', 'pred', 'y_true'])
 
-output.sort_values(by=['user_id', 'pred'], inplace=True, ascending=False)
-output = output.groupby('user_id').head(5)
-output['rank'] = output[['user_id', 'pred']].groupby('user_id').rank(method='first', ascending=False).astype(float)
-output['dcg'] = (np.power(2, output['y_true']) - 1) / np.log2(output['rank'] + 1)
+output, rmse, dcg = get_eval_metrics(output, at_k=params['eval_k'])
 
-rmse = np.sqrt(mean_squared_error(output.y_true, output.pred))
-
-print(output)
-
-avg_dcg = output.dcg.mean()
-print(rmse)
-print(avg_dcg)
+print("rmse: {:.4}".format(rmse))
+print("dcg: {:.4}".format(dcg))
