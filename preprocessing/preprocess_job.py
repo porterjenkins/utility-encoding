27727af89740e68c099b962d534cf5fe60fce2a2
Porter <porterjenkins@gmail.com>
2020-01-18 21:33:15 -0500

    enable amazon preprocessing in preprocessing_jbo

--- preprocessing/preprocess_job.py
+++ preprocessing/preprocess_job.py
@@ -13,20 +13,29 @@ import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--nrows", type = int, help="limit number of rows")
+parser.add_argument("--dataset", type = str, help = "dataset to process: {amazon, movielens")
 args = parser.parse_args()
 
 
+if args.dataset == "movielens":
 
+    df = pd.read_csv(cfg.vals['movielens_dir'] + "/ratings.csv", nrows=args.nrows)
+    df.columns = ['user_id', 'item_id', 'rating', 'timestamp']
+    out_dir = cfg.vals['movielens_dir'] + "/preprocessed/"
 
-df = pd.read_csv(cfg.vals['movielens_dir'] + "/ratings.csv", nrows=args.nrows)
-df.columns = ['user_id', 'item_id', 'rating', 'timestamp']
+elif args.dataset == "amazon":
 
-X = df[['user_id', 'item_id', 'rating']]
+    df = pd.read_csv(cfg.vals['amazon_dir'] + "/ratings.csv", nrows=args.nrows)
+    df.columns = ['user_id', 'item_id', 'rating']
+    out_dir = cfg.vals['amazon_dir'] + "/preprocessed/"
+
+else:
+    raise ValueError("--dataset must be 'amazon' or 'movielens'")
 
 arr, user_item_rating_map, item_rating_map, user_id_map, id_user_map, item_id_map, id_item_map, stats = preprocess_user_item_df(
-    X)
+    df[['user_id', 'item_id', 'rating']])
 
-out_dir = cfg.vals['movielens_dir'] + "/preprocessed/"
+
 
 write_dict_output(out_dir, "user_item_rating.json", user_item_rating_map)
 write_dict_output(out_dir, "item_rating.json", item_rating_map)
@@ -36,8 +45,14 @@ write_dict_output(out_dir, "item_id_map.json", item_id_map)
 write_dict_output(out_dir, "id_item_map.json", id_item_map)
 write_dict_output(out_dir, "stats.json", stats)
 
-arr = np.concatenate([arr, df[['rating', 'timestamp']]], axis=1)
 
-df = pd.DataFrame(arr, columns=['user_id', 'item_id', 'rating', 'timestamp'])
+if 'timestamp' in df.columns:
+
+    arr = np.concatenate([arr, df[['rating', 'timestamp']]], axis=1)
+    df = pd.DataFrame(arr, columns=['user_id', 'item_id', 'rating', 'timestamp'])
+
+else:
+    arr = np.concatenate([arr, df[['rating']]], axis=1)
+    df = pd.DataFrame(arr, columns=['user_id', 'item_id', 'rating'])
 
 df.to_csv(out_dir + "ratings.csv", index=False)
