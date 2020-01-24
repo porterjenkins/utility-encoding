9b88d059f93559c42271d40fbc325708ddbc7c9b
Porter <porterjenkins@gmail.com>
2020-01-24 16:54:22 -0500

    debug

--- model/trainer.py
+++ model/trainer.py
@@ -331,11 +331,11 @@ class SequenceTrainer(NeuralUtilityTrainer):
         if batch_size is None:
             batch_size = self.batch_size
 
-        return torch.zeros(1, batch_size, self.model.h_dim_size).to(self.device)
+        return torch.zeros(1, batch_size, self.model.h_dim_size)
 
     def fit(self):
 
-        h_init = self.init_hidden()
+        h_init = self.init_hidden().to(self.device)
 
         self.print_device_specs()
 
