87f896596e54a155d9909cbb60e54946ac5bb280
Ahmad <ahmad.k.farag@gmail.com>
2020-01-25 16:43:24 -0500

    exploration

--- explore/explore.py
+++ explore/explore.py
@@ -1,35 +1,22 @@
+from random import sample
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import torch
 from sklearn.manifold import TSNE
 
-import config.config as cfg
-from preprocessing.meta_data_item_mapper import load_mapping_for
+from preprocessing.meta_data_item_mapper import get_meta_config, MetaDataMap
 
-config = {
-    'num_items': 100,
-    'pantry': {
-        'pp': cfg.vals['amazon_dir'] + "/preprocessed_pantry/",
-        'model': cfg.vals['model_dir'] + "/pantry_ahmad/"
-    },
-    'home_kitchen': {
-        'pp': cfg.vals['amazon_dir'] + "/preprocessed_home_kitchen/",
-        'model': cfg.vals['model_dir'] + "/home_kitchen_ahmad/"
-    },
-    'grocery': {
-        'pp': cfg.vals['amazon_dir'] + "/preprocessed_grocery/",
-        'model': cfg.vals['model_dir'] + "/grocery_ahmad/"
-    }
-}
+config = get_meta_config()
+
+mm = MetaDataMap(get_meta_config())
 
 
-def load_mm_model_for(name):
+def load_model_for(name):
     model_dir = config[name]['model']
-    mm_dir = config[name]['pp']
     model = torch.load(model_dir + '/item_encoder_amazon_utility_done.pt')
-    mm = load_mapping_for(mm_dir, name)
-    return mm, model
+    return model
 
 
 def load_items(name):
@@ -40,26 +27,44 @@ def load_items(name):
     return items
 
 
-def map_id_to_title(mm, idx):
-    return mm[mm[(str(int(idx)))]]  # lol float -> int -> string -> asin -> title
+def map_id_to_title(name, idx):
+    nm = mm.get_id_asin(name)
+    return nm[nm[(str(int(idx)))]]  # lol float -> int -> string -> asin -> title
+
+
+def map_asin_id(name, asin):
+    return mm.get_idx_for_asin(name, asin)
+
+
+def map_id_to_cat(name, idx):
+    asin = mm.get_id_asin(name)[(str(int(idx)))]
+    nm = mm.get_cat(name)
+    return nm[asin][0]
+
+
+def get_also_bought(name, idx):
+    asin = mm.get_id_asin(name)[(str(int(idx)))]
+    bought = mm.get_bought(name)
+    if asin in bought:
+        return bought[asin]
+    else:
+        return None
 
 
 def do_explore(name):
     items = load_items(name)
-    mapping, model = load_mm_model_for(name)
+    model = load_model_for(name)
 
     arr = np.empty((0, 256))
     weights = model.embedding.weights.weight.data.to('cpu')
     labels = []
     i = 0
+    items = sample(items, config['num_items'])
 
     for val in items:
         vec = weights[:, int(val)]
         arr = np.append(arr, [vec.numpy()], axis=0)
-        labels.append(map_id_to_title(mapping, val))
-        i += 1
-        if i == config['num_items']:
-            break
+        labels.append(map_id_to_cat(name, val))
 
     tsne = TSNE(n_components=2, random_state=0)
     Y = tsne.fit_transform(arr)
@@ -75,4 +80,42 @@ def do_explore(name):
     plt.show()
 
 
-# do_explore('grocery')
+def do_explore_bought(name):
+    items = load_items(name)
+    model = load_model_for(name)
+
+    arr = np.empty((0, 256))
+    weights = model.embedding.weights.weight.data.to('cpu')
+    labels = []
+    i = 0
+    items = sample(items, config['num_items'])
+
+    for val in items:
+        vec = weights[:, int(val)]
+        also_bought = get_also_bought(name, val)
+        arr = np.append(arr, [vec.numpy()], axis=0)
+        labels.append(map_id_to_title(name, val))
+
+        if also_bought is not None:
+            for b in also_bought:
+                b_id = map_asin_id(name, b)
+                if b_id is not None:
+                    vec2 = weights[:, int(b_id)]
+                    arr = np.append(arr, [vec2.numpy()], axis=0)
+                    labels.append(b)
+
+    tsne = TSNE(n_components=2, random_state=0)
+    Y = tsne.fit_transform(arr)
+    x_coords = Y[:, 0]
+    y_coords = Y[:, 1]
+    # display scatter plot
+    plt.scatter(x_coords, y_coords)
+
+    for label, x, y in zip(labels, x_coords, y_coords):
+        plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords='offset points')
+    plt.xlim(x_coords.min() + 0.00005, x_coords.max() + 0.00005)
+    plt.ylim(y_coords.min() + 0.00005, y_coords.max() + 0.00005)
+    plt.show()
+
+
+do_explore_bought('grocery')
--- preprocessing/meta_data_item_mapper.py
+++ preprocessing/meta_data_item_mapper.py
@@ -2,39 +2,150 @@ import os
 import pickle
 import sys
 
+import config.config as cfg
 from preprocessing.utils import load_dict_output, pandas_df
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+wanted_columns = ['category', 'title', 'also_buy', 'asin']
 
-def create_meta_mapping(name, pp_data_dir, meta_data_file):
-    records = get_amazon_metadata(meta_data_file)
+META_DATA_CONFIG = {
+    'metas': ['pantry', 'home_kitchen', 'grocery'],
+    'num_items': 5,
+    'dir': cfg.vals['amazon_dir'],
+    'meta_dir': cfg.vals['amazon_dir'] + "meta/",
+    'pantry': {
+        'pp': cfg.vals['amazon_dir'] + "/preprocessed_pantry/",
+        'name': 'Prime_Pantry',
+        'model': cfg.vals['model_dir'] + "/pantry_ahmad/"
+    },
+    'home_kitchen': {
+        'pp': cfg.vals['amazon_dir'] + "/preprocessed_home_kitchen/",
+        'name': 'Home_and_Kitchen',
+        'model': cfg.vals['model_dir'] + "/home_kitchen_ahmad/"
+
+    },
+    'grocery': {
+        'pp': cfg.vals['amazon_dir'] + "/preprocessed_grocery/",
+        'name': "Grocery_and_Gourmet_Food",
+        'model': cfg.vals['model_dir'] + "/grocery_ahmad/"
+    }
+}
+
+
+def create_asin_category_mapping(name, pp_data_dir, meta_df):
+    records = meta_df.to_dict('record')
+    asin_mapping = dict()
+    name = name
+    for record in records:
+        asin_mapping[record['asin']] = record['category']
+    with open(pp_data_dir + '/' + name + '_meta_id_cat_map.pt', 'wb') as f:
+        pickle.dump(asin_mapping, f)
+
+
+def create_title_id_meta_mapping(name, pp_data_dir, meta_df):
+    records = meta_df.to_dict('record')
     item_mapping = load_dict_output(pp_data_dir, 'id_item_map.json')
     asin_mapping = dict()
-    data_dir = pp_data_dir
     name = name
     for record in records:
         asin_mapping[record['asin']] = record['title']
 
     item_mapping.update(asin_mapping)
-    with open(data_dir + '/' + name + '_meta_map.pt', 'wb') as f:
+    with open(pp_data_dir + '/' + name + '_meta_id_item_mapping.pt', 'wb') as f:
         pickle.dump(item_mapping, f)
 
 
-@staticmethod
-def get_amazon_metadata(m_file):
+def create_asin_bought_mapping(name, pp_data_dir, meta_df):
+    records = meta_df.to_dict('record')
+    asin_mapping = dict()
+    name = name
+    for record in records:
+        asin_mapping[record['asin']] = record['also_buy']
+    with open(pp_data_dir + '/' + name + '_meta_id_bought_mapping.pt', 'wb') as f:
+        pickle.dump(asin_mapping, f)
+
+
+def do_all_meta_mapping(name, config):
+    meta_data_file = config['meta_dir'] + 'meta_' + config[name]['name'] + ".json.gz"
+    pp_data_dir = config[name]['pp']
+    meta_df_raw = load_meta_df(meta_data_file)
+    columns = meta_df_raw.columns.to_list()
+
+    mapping_cols = ['title', 'asin']
+    title_id_df = meta_df_raw[mapping_cols]
+    create_title_id_meta_mapping(name, pp_data_dir, title_id_df)
+
+    if 'category' in columns:
+        print("Categories Found")
+        mapping_cols = ['asin', 'category']
+        cat_df = meta_df_raw[mapping_cols]
+        create_asin_category_mapping(name, pp_data_dir, cat_df)
+    else:
+        print("No meta found")
+
+    if 'also_buy' in columns:
+        print("Also Bought Found")
+        mapping_cols = ['asin', 'also_buy']
+        b_df = meta_df_raw[mapping_cols].dropna()
+        create_asin_bought_mapping(name, pp_data_dir, b_df)
+
+
+def load_meta_df(m_file):
     print("getting data from {}".format(m_file))
-    df = pandas_df(m_file)
-    df = df[['title', 'asin']]
-    return df.to_dict('record')
+    return pandas_df(m_file)
 
 
-def load_mapping_for(data_dir, name):
-    with open(data_dir + '/' + name + '_meta_map.pt', 'rb') as f:
-        mm = pickle.load(f)
-    return mm
+def get_meta_config():
+    return META_DATA_CONFIG
 
-# dir = cfg.vals['amazon_dir']
-# m_file = dir + "meta/meta_Home_and_Kitchen.json.gz"
-# p_file = dir + "preprocessed_home_kitchen/"
-# create_meta_mapping('home_kitchen', p_file, m_file)
+
+def do_all_meta():
+    config = get_meta_config()
+    for cat in config['metas']:
+        do_all_meta_mapping(cat, config)
+
+
+class MetaDataMap:
+    def __init__(self, config):
+        self.config = config
+        self.meta_files = dict()
+
+        for meta in self.config['metas']:
+            data = self.config[meta]
+            pp = data['pp']
+            self.meta_files[meta] = {}
+            self.meta_files[meta]['bought'] = pp + '/' + meta + '_meta_id_bought_mapping.pt'
+            self.meta_files[meta]['id'] = pp + '/' + meta + '_meta_id_item_mapping.pt'
+            self.meta_files[meta]['cat'] = pp + '/' + meta + '_meta_id_cat_map.pt'
+            self.meta_files[meta]['idx'] = load_dict_output(pp, 'item_id_map.json')
+
+    def get_avail(self):
+        return self.config['metas']
+
+    def get_id_asin(self, meta):
+        meta_file = self.meta_files[meta]['id']
+        return self.load_mapping_for(meta_file)
+
+    def get_bought(self, meta):
+        meta_file = self.meta_files[meta]['bought']
+        return self.load_mapping_for(meta_file)
+
+    def get_cat(self, meta):
+        meta_file = self.meta_files[meta]['cat']
+        return self.load_mapping_for(meta_file)
+
+    def get_idx_for_asin(self, meta, asin):
+        if asin in self.meta_files[meta]['idx']:
+            return self.meta_files[meta]['idx'][asin]
+        else:
+            return None
+
+    def load_mapping_for(self, file):
+        from os import path
+        if path.exists(file):
+            with open(file, 'rb') as f:
+                mm = pickle.load(f)
+            return mm
+        else:
+            return {}
