76e37db4d93fe923ce84d2d662a8e1848dd2001d
Porter <porterjenkins@gmail.com>
2020-01-31 12:24:24 -0500

    generalize utility loss

--- model/_loss.py
+++ model/_loss.py
@@ -9,12 +9,13 @@ def loss_mse(y_true, y_hat):
     return mse
 
 
-def utility_loss(y_hat, y_hat_c, y_hat_s, y_true, y_true_c, y_true_s, func):
-    err = func(y_true, y_hat)
-    err_c = func(y_true_c, y_hat_c)
-    err_s = func(y_true_s, y_hat_s)
+def utility_loss(y_hat, y_hat_c, y_hat_s, y_true, y_true_c, y_true_s, loss_func):
 
-    return (err + err_c + err_s) / 3.0
+    y_true_all = torch.cat((y_true.flatten(), y_true_c.flatten(), y_true_s.flatten()))
+    y_hat_all = torch.cat((y_hat.flatten(), y_hat_c.flatten(), y_hat_s.flatten()))
+
+    return loss_func(y_true_all, y_hat_all)
+
 
 def mrs_loss(utility_loss, x_grad, x_c_grad, x_s_grad, lmbda=1):
     mrs_c = -(x_grad / x_c_grad)
