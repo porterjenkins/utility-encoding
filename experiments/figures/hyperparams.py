8f6227c802098912f1a0dafbf7c51df63d01c329
Porter <porterjenkins@gmail.com>
2020-02-04 09:55:54 -0500

    adjust font size for figures

--- experiments/figures/hyperparams.py
+++ experiments/figures/hyperparams.py
@@ -7,16 +7,19 @@ import seaborn as sns
 
 movie_logit = pd.read_csv("tuning-movie-logit.csv")
 
-fig,ax=plt.subplots()
-ax.set_xlabel("lambda",fontsize=14)
-ax.set_ylabel("HR@5",color="blue",fontsize=14)
+fig,ax=plt.subplots(figsize=(10,6))
+ax.set_xlabel("lambda",fontsize=20)
+ax.set_ylabel("HR@5",color="blue",fontsize=20)
 ax.set_ylim([.5,1])
 ax.plot(movie_logit['lambda'], movie_logit['Hit Ratio'], marker='o', color='b')
+ax.tick_params(axis='both', which='major', labelsize=16)
+
 
 ax2=ax.twinx()
 # make a plot with different y-axis using second axis object
 ax2.plot(movie_logit['lambda'],  movie_logit['NDCG'],color="red",marker="o")
-ax2.set_ylabel("NCDG@5",color="red",fontsize=14)
+ax2.set_ylabel("NCDG@5",color="red",fontsize=20)
+ax2.tick_params(axis='both', which='major', labelsize=16)
 ax2.set_ylim([.5,1])
 
 plt.savefig("tuning-movie-logit.pdf")
@@ -28,16 +31,20 @@ plt.savefig("tuning-movie-logit.pdf")
 movie_ranking = pd.read_csv("tuning-movie-rating.csv")
 
 
-fig,ax=plt.subplots()
-ax.set_xlabel("lambda",fontsize=14)
-ax.set_ylabel("MSE",color="blue",fontsize=14)
+fig,ax=plt.subplots(figsize=(10,6))
+ax.set_xlabel("lambda",fontsize=20)
+ax.set_ylabel("MSE",color="blue",fontsize=20)
 ax.set_ylim([0,6])
+ax.tick_params(axis='both', which='major', labelsize=16)
+
 ax.plot(movie_ranking['lambda'], movie_ranking['MSE'], marker='o', color='b')
 
 ax2=ax.twinx()
 # make a plot with different y-axis using second axis object
-ax2.plot(movie_ranking['lambda'],  movie_ranking['DCG'], color="red",marker="o")
-ax2.set_ylabel("CDG@5",color="red",fontsize=14)
+
+ax2.set_ylabel("CDG@5",color="red",fontsize=20)
 ax2.set_ylim([5,14])
+ax2.tick_params(axis='both', which='major', labelsize=16)
+ax2.plot(movie_ranking['lambda'],  movie_ranking['DCG'], color="red",marker="o")
 
 plt.savefig("tuning-movie-rating.pdf")
